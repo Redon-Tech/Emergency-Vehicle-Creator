2e4c8caf8f1c9f1a5fa89c258bac697a436bf703
Parker <35582919+parker02311@users.noreply.github.com>
2023-11-28 08:43:04 -1000

    Start work on settingsCoverter

---
+++ src/Utils/settingsConverter.lua
@@ -0,0 +1,9 @@
+local settingsConverter = {}
+
+function settingsConverter.updateSettings(settings: ModuleScript)
+	local pluginSettings = require(settings)
+
+	-- Uhhhhhh idk how I am gonna make this
+end
+
+return settingsConverter
